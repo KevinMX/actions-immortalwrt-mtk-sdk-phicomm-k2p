f9b6b64b3a2b4746cb7f4de7459be0c2ef858e82
Kevin.MX <17025286+KevinMX@users.noreply.github.com>
2022-08-14 23:32:33 +0800

    diy-part2: fix LAN IP for Immortal

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify default Hostname
 sed -i 's/ImmortalWrt/K2P/g' package/base-files/files/bin/config_generate
